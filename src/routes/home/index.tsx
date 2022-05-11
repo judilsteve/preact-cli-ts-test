9fe0bfb18d7ea8e3f3287ac40a82085b0e81aef2
james <judilsteve>
2022-05-11 17:55:23 +0800

    Reproduce the bug.

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,11 +1,13 @@
 import { FunctionalComponent, h } from 'preact';
 import style from './style.css';
+import { Header } from 'semantic-ui-react';
 
 const Home: FunctionalComponent = () => {
     return (
         <div class={style.home}>
             <h1>Home</h1>
             <p>This is the Home component.</p>
+            <Header>This is a header</Header>{/* IDE/build error: JSX element type 'Header' does not have any construct or call signatures. ts(2604) */}
         </div>
     );
 };
